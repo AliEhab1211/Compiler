637695d5ed5842e0af19c33155bc802137df1301
AliEhab1211 <96737401+AliEhab1211@users.noreply.github.com>
2022-05-21 08:30:18 +0200

    Add files via upload

---
+++ viewModel/EditorAndFileScannerOutPut.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Compiler2.Models;
+namespace Compiler2.viewModel
+{
+    public class EditorAndFileScannerOutPut
+    {
+        public List<Token> EditorScannerOutPut { get; set; }
+        public List<Token> FileScannerOutPut { get; set; }
+        public int EditorNumberOfErors { get; set; }
+        public int FileNumberOfErors { get; set; }
+    }
+}
---
+++ viewModel/Tokens.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Compiler2.Models;
+namespace Compiler2.viewModel
+{
+    public class Tokens
+    {
+        public List<Token> tokens { get; set; }
+
+    }
+}
